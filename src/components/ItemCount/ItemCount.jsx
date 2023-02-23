4f12da0ef1c36d69fa8dd97db81d12ac0a067c81
Alejandro Cabral <aomcabral@gmail.com>
2023-02-22 23:10:53 -0300

    Proyecto final

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 
 function ItemCount({ onAddToCart, stock }) {
-    const [count, setCount] = useState (0);
+    const [count, setCount] = useState (1);
 
     function handleAdd(){
         if(count < stock)
